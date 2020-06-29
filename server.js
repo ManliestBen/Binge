a6d91e2160740b85f4fff9299472b06f42fd1341
ManliestBen <benjamintmanley@gmail.com>
2020-06-29 16:40:11 -0500

    database up

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const logger = require('morgan');
+const port = process.env.PORT || 3001;
+// This is where our routers go
+
+
+require('dotenv').config();
+require('./config/database');
+
+app.use(logger('dev'));
+app.use(express.json());
+
+// This is where our routes go
+
+
+app.listen(port, ()=> {
+    console.log(`Express is listening on port ${port}.`)
+});
