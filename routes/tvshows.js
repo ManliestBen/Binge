45055e6f14267ec6711a9a3e6f2bddcd5ce4853c
ManliestBen <benjamintmanley@gmail.com>
2020-06-30 14:17:20 -0500

    add tvshow routes/functions

--- routes/tvshows.js
+++ routes/tvshows.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const tvshowsCtrl = require('../controllers/tvshows');
 
-// This is where our TV show routes will go
+router.get('/', tvshowsCtrl.index);
+router.get('/:id', tvshowsCtrl.show);
+router.post('/', tvshowsCtrl.create);
+router.put('/:id', tvshowsCtrl.update);
+router.delete('/:id', tvshowsCtrl.delete);
 
 module.exports = router;
