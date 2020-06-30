d3950a9d7c297242b5c1750615f5a6ad696bb053
ManliestBen <benjamintmanley@gmail.com>
2020-06-30 10:47:21 -0500

    add remaining movie functions

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,7 +2,10 @@ const Movie = require('../models/movie');
 
 module.exports = {
     create,
-    index
+    index,
+    delete: deleteOne,
+    show,
+    update
 }
 
 function create(req, res) {
@@ -15,5 +18,22 @@ function index(req, res) {
     Movie.find({})
     .then(movies => {res.json(movies)})
     .catch(err => {res.json(err)})
+}
 
+function deleteOne(req, res) {
+    Movie.findByIdAndDelete(req.params.id)
+    .then(movie => {res.json(movie)})
+    .catch(err => {res.json(err)})
+}
+
+function show(req, res) {
+    Movie.findById(req.params.id)
+    .then(movie => {res.json(movie)})
+    .catch(err => {res.json(err)})
+}
+
+function update(req, res) {
+    Movie.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    .then(movie => {res.json(movie)})
+    .catch(err => {res.json(err)})
 }
--- routes/movies.js
+++ routes/movies.js
@@ -2,5 +2,9 @@ const router = require('express').Router();
 const moviesCtrl = require('../controllers/movies');
 
 router.post('/', moviesCtrl.create);
-router.get('/', moviesCtrl.index)
+router.get('/', moviesCtrl.index);
+router.delete('/:id', moviesCtrl.delete);
+router.get('/:id', moviesCtrl.show);
+router.put('/:id', moviesCtrl.update)
+
 module.exports = router;
