924a9e720205464fef4bb101bfc91f311cca273d
ManliestBen <benjamintmanley@gmail.com>
2020-07-06 10:16:49 -0500

    configure React Router

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './pages/App/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <Route render={({history}) => <App history={history} />} />
+    </React.StrictMode>
+  </Router>,
   document.getElementById('root')
 );
 
